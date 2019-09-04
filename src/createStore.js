515da708532987b212a16a0ec2232bcebb5a1ac1
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-09-04 17:33:36 +0300

    description + tests fix

--- src/createStore.js
+++ src/createStore.js
@@ -3,7 +3,15 @@
 /**
  * Implement `createStore` function:
  *
- * Function takes 2 arguments and returns an object
+ * Function takes 2 arguments: `recuder` and `initialState` and returns
+ * an object with 3 methods:
+ *
+ * 1. `getState` returns current state
+ * 2. `dispatch` accepts an `action` object. It runs the `reducer` passing
+ * current `state` and the `action`. New state returned by the `reducer`
+ * is saved to be returned by `getState`.
+ * 3. `subscribe` accepts a callback. All the added callbacks are run without
+ * arguments each time after the `dispatch` updates the `state`.
  *
  * Example:
  * `createStore(numberReducer, 0)` === `{...}`
--- src/createStore.test.js
+++ src/createStore.test.js
@@ -6,29 +6,42 @@ const createStore = require('./createStore');
   // functions
   const reducer = jest.fn(() => ({ count: 1 }));
   const callback = jest.fn();
+  const callback2 = jest.fn();
 
   // store
   const initialState = { count: 0 };
   const store = createStore(reducer, initialState);
 
   store.subscribe(callback);
+  store.subscribe(callback2);
   store.dispatch({ type: 'increase' });
   store.dispatch({ type: 'decrease' });
 
-  test('Reducer should be called twice', () => {
-    expect(reducer.mock.calls.length).toBe(2);
+  describe('getState method', () => {
+    test('Store should return an object with the current state', () => {
+      expect(store.getState()).toEqual({ count: 1 });
+    });
   });
 
-  test('Store should return an object with state', () => {
-    expect(store.getState()).toMatchObject({ count: 1 });
-  });
+  describe('Dispatch & subscribe methods', () => {
+    test('Reducer should be n in every dispatch', () => {
+      expect(reducer.mock.calls.length).toBe(2);
+    });
 
-  test('Reducer expects two arguments', () => {
-    expect(reducer).toBeCalledWith({ count: 0 }, { type: 'increase' });
-    expect(reducer).toBeCalledWith({ count: 1 }, { type: 'decrease' });
-  });
+    test('Reducer should take state and action for the first call', () => {
+      expect(reducer).toBeCalledWith({ count: 0 }, { type: 'increase' });
+    });
 
-  test('Callback should be called after each dispatch', () => {
-    expect(callback.mock.calls.length).toBe(2);
+    test('Reducer should take updated state for the second call', () => {
+      expect(reducer).lastCalledWith({ count: 1 }, { type: 'decrease' });
+    });
+
+    test('Each callback should be called after each dispatch', () => {
+      expect(callback.mock.calls.length).toBe(2);
+    });
+
+    test('Dispatch should call each callback', () => {
+      expect(callback2.mock.calls.length).toBe(2);
+    });
   });
 }
